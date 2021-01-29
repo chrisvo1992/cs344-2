8f8435d019f02706e8aa87ab40d0e8cc70fec8e9
Ende Alv <johnsal@oregonstate.edu>
2021-01-29 14:05:14 -0800

    get the fkn list woroking dood

--- a3/smallsh.c
+++ a3/smallsh.c
@@ -3,22 +3,90 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_LEN 2048
 #define MAX_ARGS 512
 
-void parse(char* cmd, char** val) {
-	while(*cmd == ' ' || *cmd == '\t' || *cmd == '\n') {
-		*cmd++ = '\0';
-		*val++ = cmd;
-		while(*cmd != '\0' && *cmd != ' ' && *cmd != '\t' && *cmd != '\n') {
-			cmd++;
-		}
-		*val = '\0';
-	}
+///////////////////////////////////////////////////////////////////////////////
+// a linked list that represents each command entered on a single line
+// type defined to CMD
+struct node 
+{
+	char* val;
+	struct node* prev;
+	struct node* next;
+};
+//typedef node CMD;
+
+///////////////////////////////////////////////////////////////////////////////
+// create a command for a doubly linked list of commands.
+// input: a token representing each command that has been 
+// 	separated by a space.
+// output: a node representing an individual command.
+struct node* CreateCommandNode(char* str) 
+{
+	struct node* newCommand = (struct node*)malloc(sizeof(struct node));
+	newCommand->val = calloc(strlen(str), sizeof(char));
+	strcpy(newCommand->val, str);
+	newCommand->prev = NULL;
+	newCommand->next = NULL;
+
+	//printf("%s\n", newCommand->val);
+
+	return newCommand;
 }
 
-void run_command(char** val) {
+///////////////////////////////////////////////////////////////////////////////
+// create a command for a doubly linked list of commands
+void CreateCommandList(char* str, int* cmdCount) 
+{
+	struct node* newCmd;
+	struct node* head = NULL;
+	struct node* tail = NULL;
+	struct node* curr;
+	char* savePtr;
+	
+	//printf("Number of commands entered: %d\n", *cmdCount);
+
+	char* token = strtok_r(str, " ", &savePtr);
+
+	newCmd = CreateCommandNode(token); 
+	
+	tail = newCmd;
+	head = tail;
+
+	while(token) 
+	{
+		token = strtok_r(NULL, " ", &savePtr);
+		newCmd = CreateCommandNode(token); 
+		//printf("%s\n", newCmd->val);
+		curr = tail;
+		tail = newCmd;
+		curr->next = tail;
+		tail->next = NULL;	
+	}
+
+	/*
+	while(head != NULL)
+	{
+		printf("%s\n", head->val);
+		head = head->next;
+	}
+	*/
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+void parse(char* str) 
+{
+	printf("%s\n", str);	
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void run_command(char** val) 
+{
 	pid_t pid;
 	int status;
 
@@ -44,20 +112,22 @@ void run_command(char** val) {
 	}
 }
 
-int main() {
-
+///////////////////////////////////////////////////////////////////////////////
+int main() 
+{
 	char input[MAX_LEN + 1] = "";	
+	int count = 0;
 
 	// Enter the smallsh command line
 	while(1) {
 		printf(": ");
 		// read the input, skipping white-space (space, tab, newline, etc.)
-		if(scanf("%s", input) < 1) {
+		count = scanf("%s", input);
+		if(count < 1) {
 			printf("Yea, no...\n");
 		}
-		printf("%s\n", input);
-		//parse(input, argv);
-		//run_command(argv);	
+		CreateCommandList(input, &count);
 	}
+
 	return 0;
 }
