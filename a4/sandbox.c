90071e22b87ad9c7e62f5c08aaee5d005355818c
Ende <30739491+endepointe@users.noreply.github.com>
2021-02-16 23:39:31 -0800

    Update sandbox.c

--- a4/sandbox.c
+++ a4/sandbox.c
@@ -184,6 +184,7 @@ void* consumer(void* args) {
 */
 
 // Producer-Consumer Using Condition variable
+/*
 int buffer[SIZE];
 // number of items in the buffer, shared resource
 int count = 0;
@@ -275,10 +276,134 @@ void *consumer(void* args) {
 	}
 	return NULL;
 }
+*/
+// Size of the buffer
+#define SIZE 15
+
+// Number of items that will be produced before the END_MARKER. Note that this number is smaller than the size of the buffer. This means that we can model the buffer as unbounded
+#define NUM_ITEMS 12
+
+// Special marker used to indicate end of the producer data
+#define END_MARKER -1
+
+// Buffer, shared resource
+int buffer[SIZE];
+// Number of items in the buffer, shared resource
+int count = 0;
+// Index where the producer will put the next item
+int prod_idx = 0;
+// Index where the consumer will pick up the next item
+int con_idx = 0;
+
+// Initialize the mutex
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Initialize the condition variables
+pthread_cond_t full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
+
+/*
+Produces a random integer between [0, 1000] unless it is the last item to be produced in which case the value -1 is returned.
+*/
+int produce_item(int i){
+  int value;
+  if (i == NUM_ITEMS)
+      value = END_MARKER;
+  else
+      value = rand() % 1000;
+  return value;
+}
+
+/*
+ Put an item in the shared buffer
+*/
+int put_item(int value)
+{
+    buffer[prod_idx] = value;
+    // Increment the index where the next item will be put. Roll over to the start of the buffer if the item was placed in the last slot in the buffer
+    prod_idx = (prod_idx + 1) % SIZE;
+    count++;
+    return value;
+}
+
+/*
+ Function that the producer thread will run. Produce an item and put in the buffer only if there is space in the buffer. If the buffer is full, then wait until there is space in the buffer.
+*/
+void *producer(void *args)
+{
+    for (int i = 0; i < NUM_ITEMS + 1; i++)
+    {
+      // Produce the item outside the critical section
+      int value = produce_item(i);
+      // Lock the mutex before checking where there is space in the buffer
+      pthread_mutex_lock(&mutex);
+      while (count == SIZE)
+        // Buffer is full. Wait for the consumer to signal that the buffer has space
+        pthread_cond_wait(&empty, &mutex);
+      put_item(value);
+      // Signal to the consumer that the buffer is no longer empty
+      pthread_cond_signal(&full);
+      // Unlock the mutex
+      pthread_mutex_unlock(&mutex);
+      // Print message outside the critical section
+      printf("PROD %d\n", value);
+    }
+    return NULL;
+}
+
+/*
+ Get the next item from the buffer
+*/
+int get_item()
+{
+    int value = buffer[con_idx];
+    // Increment the index from which the item will be picked up, rolling 
+    // over to the start of the buffer if currently at the end of the buffer
+    con_idx = (con_idx + 1) % SIZE;
+    count--;
+    return value;
+}
+
+/*
+ Function that the consumer thread will run. Get  an item from the buffer if 
+ the buffer is not empty. If the buffer is empty then wait until there is data in the buffer.
+*/
+void *consumer(void *args)
+{
+    int value = 0;
+    // Continue consuming until the END_MARKER is seen    
+    while (value != END_MARKER)
+    {
+      // Lock the mutex before checking if the buffer has data      
+      pthread_mutex_lock(&mutex);
+      while (count == 0)
+        // Buffer is empty. Wait for the producer to signal that the buffer has data
+        pthread_cond_wait(&full, &mutex);
+      value = get_item();
+      // Signal to the producer that the buffer has space
+      pthread_cond_signal(&empty);
+      // Unlock the mutex
+      pthread_mutex_unlock(&mutex);
+      // Print the message outside the critical section
+      printf("CONS %d\n", value);
+    }
+    return NULL;
+}
 
 int main(int argc, char* argv[])
 {
+	// unbounded 
+	srand(time(0));
+    	pthread_t p, c;
+    	// Create the producer thread
+    	pthread_create(&p, NULL, producer, NULL);
+    	// Now create the consumer thread
+    	pthread_create(&c, NULL, consumer, NULL);
+    	pthread_join(p, NULL);
+    	pthread_join(c, NULL);
+	
 	// producer consumer using conditional variable
+	/*
 	if (argc != 2) {
 		printf("usage: ./sandbox number of items\n");
 		exit(1);
@@ -293,6 +418,7 @@ int main(int argc, char* argv[])
 	pthread_create(&c, NULL, consumer, NULL);
 	pthread_join(p, NULL);
 	pthread_join(c, NULL);
+	*/
 	
 	
 	//conditional variables for shared resources using a buffer,
